f74902160286982d3a6159c56acf6af1a5e138b2
TheConcours <kazuki.hamada@sie.co.jp>
2023-08-11 09:08:45 +0900

    Created data/15

---
+++ scripts/16/mcd.bat
@@ -0,0 +1,5 @@
+:: mcd.bat
+:: 
+@echo off
+if NOT exist %1 (mkdir %1)
+cd %1
--- scripts/18/breakschedule.ps1
+++ scripts/18/breakschedule.ps1
@@ -5,5 +5,5 @@ $TaskName = 'Break'
 $filePath = "$env:HOMEPATH\\winpractice\\scripts\\18\\breaktime.ps1"
 $time = (Get-Date).addMinutes(2).ToString("HH:mm:ss")
 $Trigger = New-ScheduledTaskTrigger -Once -At $time
-$Action= New-ScheduledTaskAction -Execute "pwsh" -Argument "$filePath" 
+$Action= New-ScheduledTaskAction -Execute "pwsh.exe " -Argument "-ExecutionPolicy Bypass $filePath" 
 Register-ScheduledTask -TaskName $TaskName -Trigger $Trigger -Action $Action
